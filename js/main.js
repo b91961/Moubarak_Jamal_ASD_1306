62b259b6417ed80f2d23c42205ad427c6b2b1470
Jamal Moubarak <jamalmoubarak@fullsail.edu>
2013-06-06 21:55:30 -0400

    Added some style and basic page init in javascript.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 // Jamal Moubarak
 // ASD 1306
 
+$(document).ready(function(){
+
+	$('#home').on('pageinit', function() {
+
+	});
+
+	$('#info').on('pageinit', function() {
+
+	});
+
+	$('#todo').on('pageinit', function() {
+
+	});
+
+	$('#addItem').on('pageinit', function() {
+
+});
